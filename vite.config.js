44380a4e49bc84be7e4dfea979fc938a04d81859
MASON-SEA <yangyangmason@outlook.com>
2024-07-12 11:01:16 +0800

    初始版本提交

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,47 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import fs from 'fs';
+
+// Helper function to get all index.tsx files in the components directory
+function getComponentEntries(dir) {
+  const entries = {};
+  const componentDirs = fs.readdirSync(dir);
+
+  componentDirs.forEach((componentDir) => {
+    const fullPath = path.join(dir, componentDir, 'index.tsx');
+    if (fs.existsSync(fullPath)) {
+      const entryName = path.join('components', componentDir, 'index');
+      entries[entryName] = fullPath;
+    }
+  });
+
+  return entries;
+}
+
+const componentEntries = getComponentEntries(path.resolve(__dirname, 'src/components'));
 
 export default defineConfig({
   build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.tsx'), // 入口文件路径
-      name: 'YSUI', // npm 包的名称
-      fileName: format => `index.${format}.js`,
-    },
     rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'src/index.tsx'),
+        ...componentEntries
+      },
+      output: [
+        {
+          format: 'cjs',
+          dir: 'dist/cjs',
+          entryFileNames: '[name].js'
+        },
+        {
+          format: 'es',
+          dir: 'dist/es',
+          entryFileNames: '[name].js'
+        }
+      ],
+      
       // 其他 rollup 配置
+      external: ['yselement'],
     },
   },
 });
